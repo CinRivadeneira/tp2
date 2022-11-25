a96bac0c7d3f7901be7802c3f2eff91f03a62d59
CinRivadeneira <111160025+CinRivadeneira@users.noreply.github.com>
2022-11-25 00:43:38 -0300

    actualizo el modo nocturno con local storage

--- Javascript/mode.js
+++ Javascript/mode.js
@@ -1,7 +1,19 @@
+const boton = document.getElementById("mode");
 
-const btnMode = document.getElementById("mode");
+boton.addEventListener("click", () => {
+    document.body.classList.toggle("darkmode");
 
-btnMode.addEventListener("click", function(){
-    document.body.classList.toggle('darkmode')
-})
+
+    // inicia local storage//
+
+if(document.body.classList.contains("darkmode")){
+    localStorage.setItem("mode", true)
+}else{localStorage.removeItem("mode");
+}
+});
+
+
+if(localStorage.getItem("mode")){
+    document.body.classList.add("darkmode");
+}
 
