1cc12ff5d00a358ecf83b42dd34d9f731367b4af
CinRivadeneira <cistri092@gmail.com>
2023-01-23 12:19:35 -0300

    navbar responsive

--- tucweb/src/Components/Navbar.jsx
+++ tucweb/src/Components/Navbar.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
 import "../css/navbar.css";
 
 
 const Navbar = () => {
+    const [showLinks, setShowLinks] = useState (true);
+
     return (
         <div className="navbar">
             <h1> logo </h1>
             <div className="leftSide">
-                <div className="links">
+                <div className="links" id={showLinks ? "hidden" : ""} >
                     <a href='/home'>Inicio</a>
                     <a href="/quienessomos">Quienes somos</a>
                     <a href="/contacto">Contacto</a>
                     <a href="/usuarios">Usuarios</a>
                 </div>
+                <button> open </button>
             </div>
         </div>
     )
