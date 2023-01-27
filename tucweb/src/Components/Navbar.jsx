449035464087e15bc2c49b87d0349f5faa7908e1
CinRivadeneira <cistri092@gmail.com>
2023-01-27 13:42:37 -0300

    agrego icon close

--- tucweb/src/Components/Navbar.jsx
+++ tucweb/src/Components/Navbar.jsx
@@ -1,27 +1,26 @@
-import React, { useState } from 'react'
-import "../Css/Navbar.css";
-import { Redirect } from 'react-router-dom';
-import ReorderIcon from "@mui/icons-material/Reorder";
-import logo from "../Img/Banner.png";
+import React from 'react';
+import "../Css/Navbar.css"
+import Banner from "../Img/Banner.png";
+import Hamburger from "../Img/Icons/Hamburger.png";
+import Close from "../Img/Icons/Close.png";
 
 const Navbar = () => {
-    const [showLinks, setShowLinks] = useState(false);
-    const [reorderIcon, setReorderIcon] = useState(false);
-
     return (
-        <div className="navbar">
+        <div className='Navbar'>
             <div className='logo'>
-                <img src={logo} />
+                <img src={Banner} />
             </div>
-            <div className="menu">
-                <div className="links" id={showLinks ? "hidden" : ""} >
-                    <a href='/inicio'>Inicio</a>
-                    <a href="/quienessomos">Quienes somos</a>
-                    <a href="/contacto">Contacto</a>
-                    <a href="/usuarios">Usuarios</a>
-                </div>
-                <button className="mobileMenu" onClick={() => setShowLinks(!showLinks)}> <ReorderIcon onClick={() => setReorderIcon(!reorderIcon)} />
-                </button>
+            <button className='open-menu' aria-label="Abrir menú">
+                <img src= {Hamburger} />
+            </button>
+            <button className='close-menu' aria-label="Cerrar menú">
+                <img src= {Close} />
+            </button>
+            <div className='Menu'>
+                <a href='/inicio'>Inicio</a>
+                <a href="/quienessomos">Quienes somos</a>
+                <a href="/contacto">Contacto</a>
+                <a href="/usuarios">Usuarios</a>
             </div>
         </div>
     )
