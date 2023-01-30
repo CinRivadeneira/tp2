055f97b73bf5d89b369ce3fecacd8e24c6837f22
CinRivadeneira <111160025+CinRivadeneira@users.noreply.github.com>
2023-01-30 02:22:51 -0300

    Navbar first mobile

--- tucweb/src/Components/Navbar.jsx
+++ tucweb/src/Components/Navbar.jsx
@@ -1,33 +1,61 @@
-import React, { useState } from 'react'
-import "../Css/Navbar.css";
-import { Redirect } from 'react-router-dom';
-import ReorderIcon from "@mui/icons-material/Reorder";
-import Banner from "../Img/Banner.png";
+import React, { useState } from 'react';
+import { Container, LogoContainer, Wrapper, Menu, MenuItem, MenuItemLink, MobileIcon } from './Navbar.elements';
+import Banner from "../Img/Banner.png"
+import { FaBars, FaHome, FaUsers, FaMailBulk, FaRegIdCard, FaTimes } from "react-icons/fa";
 
 const Navbar = () => {
-    const [showLinks, setShowLinks] = useState(false);
-    const [reorderIcon, setReorderIcon] = useState(false);
+    const [showMobileMenu, setShowMobileMenu] = useState(false)
 
     return (
-        <div className="navbar">
-            <div className='logo'>
-                <img src={Banner} />
-            </div>
-            <div className="menu">
-                <div className="links" id={showLinks ? "hidden" : ""} >
-                    <a href='/inicio'>Inicio</a>
-                    <a href="/quienessomos">Quienes somos</a>
-                    <a href="/contacto">Contacto</a>
-                    <a href="/usuarios">Usuarios</a>
-                </div>
-                <button className="mobileMenu" onClick={() => setShowLinks(!showLinks)}> <ReorderIcon onClick={() => setReorderIcon(!reorderIcon)} />
-                </button>
-            </div>
-        </div>
+        <Container>
+            <Wrapper>
+                <LogoContainer>
+                    <img src={Banner} alt="Logo" />
+                </LogoContainer>
+
+
+                <MobileIcon onClick={() => setShowMobileMenu(!showMobileMenu)}>
+                    {showMobileMenu ? <FaTimes /> : <FaBars />
+                    }
+                
+                </MobileIcon>
+                <Menu open={showMobileMenu} >
+                    <MenuItem>
+                        <MenuItemLink href='/inicio'>
+                            <div>
+                                <FaHome />
+                            </div>
+                            Inicio
+                        </MenuItemLink>
+                    </MenuItem>
+                    <MenuItem>
+                        <MenuItemLink href="/quienessomos">
+                            <div>
+                                <FaUsers />
+                            </div>
+                            Quienes somos
+                        </MenuItemLink>
+                    </MenuItem>
+                    <MenuItem>
+                        <MenuItemLink href="/contacto">
+                            <div>
+                                <FaMailBulk />
+                            </div>
+                            Contacto
+                        </MenuItemLink>
+                    </MenuItem>
+                    <MenuItem>
+                        <MenuItemLink href="/usuarios">
+                            <div>
+                                <FaRegIdCard />
+                            </div>
+                            Usuarios
+                        </MenuItemLink>
+                    </MenuItem>
+                </Menu>
+            </Wrapper>
+        </Container>
     )
 }
 
 export default Navbar;
-
-
-
