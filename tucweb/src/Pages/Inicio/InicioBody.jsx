7e22881a9ef789c61877d9ba10323beb51b0a7c4
CinRivadeneira <111160025+CinRivadeneira@users.noreply.github.com>
2023-01-30 19:29:48 -0300

    Comienzo a agregar inicio

--- tucweb/src/Pages/Inicio/InicioBody.jsx
+++ tucweb/src/Pages/Inicio/InicioBody.jsx
@@ -1,1 +1,29 @@
 
+import React from 'react';
+import "../../Css/Style.css"
+
+const InicioBody = () => {
+    return (
+        <>
+            <br /> <br />
+            <section className="introduccion"> 
+                <h2> Deje su empresa o emprendimiento en nuestras manos.
+                    <br></br>
+                    <br></br>
+                    Ayudamos a conseguir tus objetivos de alcance posicionando tu marca en los primeros resultados de búsqueda.
+                </h2>
+            </section>
+            <br /> <br />
+            <h1> NUESTROS SERVICIOS </h1>
+            <br /> <br />
+
+
+
+
+
+
+
+        </>
+    )
+}
+export default InicioBody;
